c1ddd44afa08f5f7d939a72d19116f06822dc61e
yyngfive <chenhye5@outlook.com>
2024-08-18 22:06:14 +0800

    更新了LICENSE

--- assets/utils/fileType.ts
+++ assets/utils/fileType.ts
@@ -1,3 +1,20 @@
+// Copyright (C) 2024  yyngfive 
+
+// Email: chenhye5@outlook.com
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import type { FileInfo } from "@/types/parser";
 
 export function getFileType(filename: string): FileInfo["fileType"] {
@@ -19,7 +36,7 @@ const fileExtensions: Record<string, FileInfo["fileType"]> = {
   docx: "word",
   xls: "excel",
   xlsx: "excel",
-  jpg:"figure",
-  png:"figure",
-  jpeg:"figure",
+  jpg: "figure",
+  png: "figure",
+  jpeg: "figure",
 };
