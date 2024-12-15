52462856f4fc2a4f116d2646b84a771d845eccda
yyngfive <chenhye5@outlook.com>
2024-12-15 14:54:13 +0800

    新增了changelog页面

--- entrypoints/changelog/main.tsx
+++ entrypoints/changelog/main.tsx
@@ -17,11 +17,11 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import ChangeLog from './ChangeLog.tsx';
 import '@/assets/css/style.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChangeLog />
   </React.StrictMode>,
 );
